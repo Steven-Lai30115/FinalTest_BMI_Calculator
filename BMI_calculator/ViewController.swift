5a84b31c7400cfc699f428149401ba53dd2d01f5
Steven Lai <wai30115@live.hk>
2022-12-05 23:06:21 -0500

    Project setup

---
+++ BMI_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_calculator
+//
+//  Created by chin wai lai on 5/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
